9aa0f33f2edc07929c7447ddac15efb48272cf9b
louistran2k <louistran2k>
2022-12-10 03:45:16 +0700

    2

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MetaDecoration from './MetaDecoration';
-import thumbnailImage from './assets/thumbnail1.png';
+import thumbnailImage from './assets/thumbnail1.jpg';
 
 function App() {
   return (
