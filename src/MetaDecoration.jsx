0b89a40b8de48c1afe71f6a3e5f735f90092da97
louistran2k <louistran2k>
2022-12-10 03:15:06 +0700

    fix

--- src/MetaDecoration.jsx
+++ src/MetaDecoration.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet'
 
+const hostName = 'https://louistran2k.github.io/react-app-meta-tags'
+
 const MetaDecoration = ({ title, description, imageUrl, imageAlt }) => {
   return (
     <Helmet>
       <title>{title}</title>
       <meta property="og:title" content={title} />
       <meta property="og:type" content="article" />
-      <meta property="og:image" content={imageUrl} />
-      <meta property="og:url" content={window.location.pathname + window.location.search} />
+      <meta property="og:image" content={hostName + imageUrl} />
+      <meta property="og:url" content={hostName + window.location.pathname + window.location.search} />
       <meta property="og:description" content={description} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image:alt" content={imageAlt} />
